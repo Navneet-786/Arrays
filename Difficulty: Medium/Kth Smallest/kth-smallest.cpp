fd0a16b457490a6baf762211360e4b70c08d924e
Navneet verma <vermamanu52528591@gmail.com>
2026-02-13 12:48:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.cpp
+++ Difficulty: Medium/Kth Smallest/kth-smallest.cpp
@@ -1,43 +1,42 @@
 class Solution {
-    int quickSelect(vector<int>&nums,int start, int end){
-         int pivot = nums[end];
-        int pivotIndex = start;
-         
-         
-          for(int i = start; i < end; i++){
-            if(nums[i] <= pivot){
-                swap(nums[i], nums[pivotIndex]);
-                pivotIndex++;
-            }
-        }
-
-        swap(nums[pivotIndex], nums[end]);
-        return pivotIndex;
-    }
+   int quickSelect(vector<int>&nums, int start, int end){
+       int pivot = nums[end];
+       //this index is special where all the smallest elememnt
+       //from pivot is left of pivotIndex and greater are on right of 
+       //pivot index
+       int pivotIndex =  start;
+       
+       for(int i = start;i<end;i++){
+           if(nums[i] <= pivot){
+               swap(nums[i], nums[pivotIndex]);
+               pivotIndex++;
+           }
+       }
+       
+       swap(nums[end], nums[pivotIndex]);
+       return pivotIndex;
+   }
     
   public:
-    int kthSmallest(vector<int> &arr, int k) {
-        if(k>arr.size())return -1;
+    int kthSmallest(vector<int> &nums, int k) {
+       int n = nums.size();
         
-        int n = arr.size();
-        int targetIndex = k-1;
+        //find the targetIndex
+        int targetIndex = k - 1; //kth smallest
         
-        
-       
-        int start = 0, end = n-1;
+        int start= 0,end =  n-1;
         
         while(start <= end){
+            int tempIndex = quickSelect(nums, start, end);
             
-            int ansIndex = quickSelect(arr, start,end);
-            if(ansIndex == targetIndex)return arr[ansIndex];
-            else if(ansIndex < targetIndex){
-                start = ansIndex+1;
+            if(tempIndex==targetIndex)return nums[tempIndex];
+            else if(targetIndex < tempIndex){
+                end = tempIndex - 1;
             }
             else{
-                end = ansIndex-1;
+                start = tempIndex+1;
             }
         }
-        
         return -1;
         
     }
