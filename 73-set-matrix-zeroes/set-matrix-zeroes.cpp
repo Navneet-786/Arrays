9436101e2db4a08815074aa57ae2b340f5346349
Navneet verma <vermamanu52528591@gmail.com>
2026-02-14 13:16:12 +0530

    Time: 1 ms (14.90%) | Memory: 18.6 MB (79.06%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -2,34 +2,56 @@ class Solution {
 public:
 
 void setZeroes(vector<vector<int>>& vec) {
-       vector<int>rows(vec.size(),0);
-       vector<int>cols(vec[0].size(),0);
-
-       for(int i = 0;i<vec.size();i++){
+        int col0 = 0;
+        int row0 = 0;
+        for(int i=  0;i<vec.size();i++){
+            if(vec[i][0]==0){
+                col0 = 1;
+            }
+        }
         for(int j = 0;j<vec[0].size();j++){
+            if(vec[0][j]==0){
+                row0 = 1;
+            }
+        }
+       for(int i = 1;i<vec.size();i++){
+        for(int j = 1;j<vec[0].size();j++){
             if(vec[i][j]==0){
-                rows[i] = 1;
-                cols[j] = 1;
+                vec[i][0] = 0;
+                vec[0][j] = 0;
             }
         }
        }
 
        //this is time to fill rows and cols zeros
-       for(int i = 0;i<rows.size();i++){
-            if(rows[i]==1){
-                for(int j = 0;j<vec[0].size();j++){
-                    vec[i][j] = 0;
-                }
+      for(int i = 1;i<vec.size();i++){
+        if(vec[i][0]==0){
+            for(int j = 1;j<vec[0].size();j++){
+                vec[i][j] = 0;
             }
-       }
+        }
+      }
 
-       for(int i = 0;i<cols.size();i++){
-            if(cols[i]==1){
-                for(int j = 0;j<vec.size();j++){
-                    vec[j][i] = 0;
-                }
+      for(int j = 1;j<vec[0].size();j++){
+        if(vec[0][j]==0){
+            for(int i = 1;i<vec.size();i++){
+                vec[i][j] = 0;
             }
-       }
+        }
+      }
+     
+        //make entire first row and col = 0
+        if(row0){
+            for(int i = 0;i<vec[0].size();i++){
+                vec[0][i] = 0;
+            }   
+        }
+        if(col0){
+            for(int j = 0;j<vec.size();j++){
+                vec[j][0] = 0;
+            }
+        }
+      
 
     }
 };
