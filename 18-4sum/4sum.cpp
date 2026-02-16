03aa49c2b3d44ec1f9b55954846f368abb432de6
Navneet verma <vermamanu52528591@gmail.com>
2026-02-16 13:45:33 +0530

    Time: 187 ms (8.41%) | Memory: 53.4 MB (8.97%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -4,11 +4,9 @@ using ll = long long;
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         set<vector<int>>st;
         sort(nums.begin(),nums.end());
-
         for(int i = 0;i<nums.size();i++){
             for(int j = i+1;j<nums.size();j++){
                 ll local_target = target-ll(nums[i] + nums[j]);
-
                 int start = j+1, end = nums.size()-1;
                 while(start < end){
                     ll sum = nums[start] + nums[end];
@@ -22,12 +20,10 @@ using ll = long long;
                         st.insert({nums[i], nums[j], nums[start], nums[end]});
                         start++;
                         end--;
-
                     }
                 }
             }
         }
-
         return vector<vector<int>>(st.begin(),st.end());
     }
 };
